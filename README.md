d7256f3530c9ee64fb648598a9504a8e2dce3701
farizali04 <farizali0404@gmail.com>
2024-12-17 10:38:53 +0700

    W13: Commit

--- README.md
+++ README.md
@@ -3,4 +3,27 @@
 ## Fariz Ali Muhaimin | 362358302100
 
 ### Soal 1
+![Screenshot](assets/Screenshot%20(61).png)
 
+### Soal 2
+![Screenshot](assets/Screenshot%20(62).png)
+
+### Soal 3
+Keyword yield* pada kode tersebut memiliki fungsi khusus dalam konteks async generator function:
+
+yield* digunakan untuk mendelegasikan atau menyebarkan (delegate) stream yang ada ke stream utama.
+
+Dalam konteks kode ini, yield* mengembalikan seluruh stream yang dihasilkan oleh Stream.periodic().
+
+Stream.periodic() menghasilkan stream yang mengeluarkan event setiap detik, dengan nilai warna yang berubah secara berurutan dari list colors.
+
+Dengan yield*, setiap event dari stream Stream.periodic() akan secara otomatis ditambahkan ke stream utama yang sedang dibuat.
+
+![Screenshot](assets/Screenshot%20(63).png)
+
+### Soal 4
+![Screenshot](assets/Screenshot%20(64).png)
+
+![Screenshot](assets/Screenshot%20(65).png)
+
+![Screenshot](assets/Screenshot%20(67).png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Stream_Fariz",
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.lightGreen,
       ),
       home: const StreamHomePage(),
     );
@@ -27,9 +28,34 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  Color bgColor = Colors.blueGrey;
+  late ColorStream colorStream;
+
+  void changeColor() async {
+    await for (var eventColor in colorStream.getColors()) {
+      setState(() {
+        bgColor = eventColor;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    colorStream = ColorStream();
+    changeColor();
+  }
 
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stream'),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: bgColor),
+      ),
+    );
   }
 }
