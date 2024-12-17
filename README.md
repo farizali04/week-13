05bd56fc26459105b200d43eb38ab2e27ea3f567
farizali04 <farizali0404@gmail.com>
2024-12-17 09:23:01 +0700

    W13:Jawaban Soal 1

---
+++ README.md
@@ -0,0 +1,6 @@
+# stream_fariz
+
+## Fariz Ali Muhaimin | 362358302100
+
+### Soal 1
+
---
+++ lib/stream.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.greenAccent,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal
+  ];
+
+  Stream<Color> getColors() async* {
+    yield* Stream.periodic(
+      const Duration(seconds: 1), (int t) {
+        int index = t % colors.length;
+        return colors[index];
+      }
+    );
+  }
+}
